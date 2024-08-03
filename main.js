9d77cd177a1d434610312d2c07f49ecdfa5ca93d
sa20122424 <saloxiddin201224@gmail.com>
2024-08-03 16:30:29 +0500

    salom

---
+++ main.js
@@ -0,0 +1,106 @@
+// // let letterSearch = ['non', 'asal', 'banan', 'olma', 'aziza', 'Gul', 'mashina',] 
+// // let soz =[]
+// //  for (let i = 0; i < letterSearch.length; i++ ) {
+// //     if(letterSearch.includes("n")){
+// //         letterSearch[i].push(soz)
+        
+// //     }
+// //     console.log(soz );
+// // }
+ 
+ 
+ 
+ 
+
+// let son = prompt('видите первое число')
+// let son2 = prompt("ikinchi son kiritong")
+
+// function son(params) {
+// if () {
+    
+// } else {
+
+// }
+// }
+// let num  = prompt('1 son')
+// let num2 =  prompt("2 son")
+
+// function checkNumber(num,num2) {
+//     if (num > num2) {
+//         return "num2 kota";
+//     } else if (num < num2) {
+//         return "num kishkina";
+//     } else {
+//         return "Zero";
+//     }
+// }
+
+// // Пример использования функции
+// let result = checkNumber(num,num2);
+// alert(result)  // Вывод: Negative number
+
+
+
+// дз
+function compareNumbers(a, b) {
+    if (a > b) {
+        return `${a} больше, чем ${b}`;
+    } else if (a < b) {
+        return `${b} больше, чем ${a}`;
+    } else {
+        return "Числа равны";
+    }
+}
+
+// Пример использования функции
+let num1 = +prompt("Введите первое число:");
+let num2 = +prompt("Введите второе число:");
+let result = compareNumbers(num1, num2);
+alert(result);
+// дз
+
+
+
+
+// // declarition function
+// function abu(){
+//     console.log("hello");
+
+// }
+// abu()
+
+// // expresion function
+// let num = function plus() {
+//     console.log(5 > 5);
+
+// }
+
+// num()
+
+// // arrow function
+// let sum = () => 10 -10
+// console.log(sum());
+// function compareNumbers(a) {
+//     if (a % 2 === 0) {
+//        arr.push(a) 
+//        console.log(`tog ${arr}`);
+//     } else {
+//         arr2.push(a)
+//         console.log(`juft ${arr2}`); 
+//     }
+        
+    
+// }
+// let num1 = +prompt("Введите первое число:");
+// let arr = []
+// let arr2 =[]
+// let result = compareNumbers(num1);
+
+
+
+
+
+
+
+
+
